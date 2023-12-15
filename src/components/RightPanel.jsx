55934b166dedb3beb78e420eafba33402df718d2
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2023-12-15 15:02:23 +0100

    Add Right Panel

--- src/components/RightPanel.jsx
+++ src/components/RightPanel.jsx
@@ -13,44 +13,65 @@ const defaultData = {
   artistBiography: 'Bla bla bla...'
 }
 
-export default function Rightpanel ({ data = defaultData }) {
+const defaultNext = {
+  nextBand: 'Nirvana',
+  nextSong: 'Smells Like Teen Spirit',
+  nextImageUrl: '/img/n.jpg',
+}
+
+export default function Rightpanel ({ data = defaultData, nextData = defaultNext }) {
   const { playlist, band, song, imageUrl, artistImage, artistBiography } = data
+  const {
+    nextBand,
+    nextSong,
+    nextImageUrl
+  } = nextData
 
   return (
-    <div className='bg-green-200 grow w-1/4 px-3'>
-      <div className='flex justify-between'>
+    <div className='bg-neutral-900 grow w-1/4 px-3 rounded-lg m-2 text-neutral-100'>
+      <div className='flex justify-between mt-2 mb-4'>
         <span>{playlist}</span>
-        <span><Close /></span>
+        <span className='hover:bg-neutral-700 cursor-pointer'><Close /></span>
       </div>
-      <img className='' src={imageUrl} />
-      <div className='flex justify-between'>
+      <img className='rounded-lg' src={imageUrl} />
+      <div className='flex justify-between mt-2 mb-4'>
         <div>
-          <div>{song}</div>
-          <div>{band}</div>
+          <div className='text-2xl tracking-wide font-normal hover:underline hover:cursor-pointer'>{song}</div>
+          <div className='text-neutral-400 font-light hover:text-white hover:underline hover:cursor-pointer'>{band}</div>
         </div>
         <div className='flex items-center'><Check fill='green' /><Menu /></div>
       </div>
 
-      <div className='relative'>
-        <img className='' src={artistImage} />
+      <div className='relative hover:cursor-pointer '>
+        <img className='rounded-t-lg' src={artistImage} />
         <div className='absolute top-2 left-3 text-white text-md tracking-wide'>About the Artist</div>
       </div>
-      <div>
-        <div>{band}</div>
-        <div className='flex justify-between'>
-          <div>10,000,000 monthly listeners</div>
-          <button>Follow</button>
+      <div className='text-neutral-400 hover:cursor-pointer mb-4 bg-neutral-800 rounded-b-lg'>
+        <div className='text-neutral-100 py-3 px-2 font-semibold tracking-wide'><span className='hover:underline'>{band}</span></div>
+        <div className='flex justify-between px-2'>
+          <div className='text-neutral-400 font-light text-s'>10,000,000 monthly listeners</div>
+          <button className='hover:border-neutral-400 text-xs tracking-wide text-neutral-100 font-normal border-neutral-700 border-2 px-3 py-1 rounded-full' >Follow</button>
         </div>
-        <div>{artistBiography}</div>
+        <div className='px-2 py-3 text-neutral-400 font-normal text-xs'>{artistBiography}</div>
       </div>
 
-      <div>
-        <div className='flex justify-between'>
-          <div><Music /></div>
-          <div>Next in queue</div>
-          <a href=''>Open queue</a>
+      <div className='flex justify-between items-center rounded-t-lg bg-neutral-800 pt-3 px-2'>
+        <div>Next in queue</div>
+        <a className='hover:text-neutral-100 text-xs tracking-wide text-neutral-400 font-normal' href=''>Open queue</a>
+      </div>
+
+      <div className='flex items-center bg-neutral-800 rounded-b-lg py-4 px-2 hover:bg-neutral-700'>
+        <div className='mr-3'><Music /></div>
+        <div className='flex items-center'>
+          <img className='w-10 h-10 rounded-md mr-3' src={nextImageUrl} />
+          <div className='flex bg-blue- flex-col'>
+            <div className="text-neutral-100 font-normal text-sm"><span className='hover:underline hover:cursor-pointer'>{nextSong}</span></div>
+            <div className=' text-neutral-400 font-normal text-xs'><span className='hover:underline hover:cursor-pointer'>{nextBand}</span></div>
+          </div>
         </div>
       </div>
+
+
     </div>
   )
 }
--- src/components/icons/Close.jsx
+++ src/components/icons/Close.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Close = ({ fill = 'lightgrey' }) => <svg className='h-6 w-6' fill={fill} xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z' /></svg>
+const Close = ({ fill = 'lightgrey' }) => <svg className='h-4 w-4 rounded-full' fill={fill} xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z' /></svg>
 
 export default Close
